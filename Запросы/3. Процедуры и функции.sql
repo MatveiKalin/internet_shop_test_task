74bcf0b25de0712ab0161b4d5a7c165b833093b9
Калин Матвей Викторович <kalin@MOON.LAN>
2019-07-10 19:04:03 +0500

    Появился  процедуры для курсоров под номером № 2 и № 3

--- Запросы/3. Процедуры и функции.sql
+++ Запросы/3. Процедуры и функции.sql
@@ -119,7 +119,7 @@ end;
 
 
 
-/* 3. Версия 2. Изменяем цикл.  
+/* 3. Версия 3. Изменяем цикл, а именно "exit" помещаем после "fetch".  
 Курсор, который по ИД товара одает данные: 
 название товара, 
 текущая цена товара,
@@ -139,11 +139,11 @@ declare
       goods, 
       goods_price
     where
-      goods.goods_id = goods_price.goods_id and
       goods.goods_id = v_id and
-      sysdate between goods_price.active_from and goods_price.active_to; /* Цена товара должна попадать в промежуток времени */
+      goods.goods_id = goods_price.goods_id and
+      (sysdate between goods_price.active_from and goods_price.active_to); /* Цена товара должна попадать в промежуток времени */
       
-      
+
   v_сursor_goods_info  cursor_goods_info%ROWTYPE;
   v_goods_id            goods.goods_id%type;
 begin
@@ -162,14 +162,275 @@ begin
     exit when (cursor_goods_info%notfound); 
       
     dbms_output.put_line('Название товара: ' ||  v_сursor_goods_info.goods_name ||
-             ',        текущая цена товара: ' || v_сursor_goods_info.price || 
-             ',        длина товара: ' || v_сursor_goods_info.length || 
-             ',        ширина товара: ' || v_сursor_goods_info.width || 
-             ',        высота товара: ' || v_сursor_goods_info.height);
+						 ',        текущая цена товара: ' || v_сursor_goods_info.price || 
+						 ',        длина товара: ' || v_сursor_goods_info.length || 
+						 ',        ширина товара: ' || v_сursor_goods_info.width || 
+						 ',        высота товара: ' || v_сursor_goods_info.height);
             
   end loop;
                 
   close cursor_goods_info;
+end;
+
+
+
+/* 3. Версия 4. Помещаем курсор в процедуру ---------------------------------------  
+Курсор, который по ИД товара одает данные: 
+название товара, 
+текущая цена товара,
+длинна товара,
+ширина ширина,
+высота товара. */
+create or replace procedure get_info_goods (p_goods_id  in   integer, 
+                                            refcur      out  sys_refcursor) 
+is
+
+begin
+
+  open refcur for
+    select
+      goods.goods_name,
+      goods_price.price,
+      goods.length,
+      goods.width,
+      goods.height
+    from 
+      goods, 
+      goods_price
+    where
+      goods.goods_id = p_goods_id and
+      goods.goods_id = goods_price.goods_id and
+      (sysdate between goods_price.active_from and goods_price.active_to); /* Цена товара должна попадать в промежуток времени */
+  
+end get_info_goods;
+
+
+/* Вызов процедуры между begin и end */
+declare
+  tmp$cur sys_refcursor;
+  goods_name varchar2(200);
+  price varchar2(200);
+  length varchar2(200);
+  width varchar2(200);
+  height varchar2(200);
+begin
+
+  /* Вызов происходит здесь! */
+  get_info_goods (2, tmp$cur);
+
+  loop
+    fetch 
+      tmp$cur
+    into  
+      goods_name, price, length, width, height;
+    
+    exit when tmp$cur%notfound;
+    
+    dbms_output.put_line('Название товара: ' || goods_name ||
+						 ',        текущая цена товара: ' || price || 
+						 ',        длина товара: ' || length || 
+						 ',        ширина товара: ' || width || 
+						 ',        высота товара: ' || height);
+  end loop;
+end;
+
+
+
+
+
+
+/* 2. Версия 1. 
+Показывает товар, если ввести ИД категории непосредственно, то есть, если вводится ИД категории самого первого уровня, то данный курсор не проходит по всем дочерним элементам.
+Курсор, оторый по ИД категории отдает список:  
+ИД товара, 
+Название товара,
+Текущая цена */
+declare
+  
+  cursor cursor_goods_info(v_id goods_inherit_category.goods_inherit_category_id%type) is
+    select
+      goods.goods_id,
+      goods.goods_name,
+      goods_price.price
+    from
+      category_goods,
+      goods_inherit_category, 
+      goods, 
+      goods_price
+    where
+      category_goods.category_goods_id = v_id and
+      category_goods.category_goods_id = goods_inherit_category.category_goods_id and
+      goods_inherit_category.goods_id = goods.goods_id and
+      goods.goods_id = goods_price.goods_id and
+      (sysdate between goods_price.active_from and goods_price.active_to); /* Цена товара должна попадать в промежуток времени */
+       
+      
+      
+  v_сursor_goods_info  cursor_goods_info%ROWTYPE;
+  v_category_goods_id  goods.goods_id%type;
+begin
+  
+  v_category_goods_id := 3;
+  
+  open cursor_goods_info(v_category_goods_id);
+  
+  loop
+  
+    fetch 
+      cursor_goods_info 
+    into 
+      v_сursor_goods_info;
+      
+    exit when (cursor_goods_info%notfound); 
+      
+    dbms_output.put_line('ИД товара: ' ||  v_сursor_goods_info.goods_id ||
+             ',        Название товара: ' ||  v_сursor_goods_info.goods_name ||
+             ',        текущая цена товара: ' || v_сursor_goods_info.price);
+            
+  end loop;
+                
+  close cursor_goods_info;
+end;
+
+
+
+
+/* 2. Версия 2. Помещаем курсор в процедуру --------------------------------------- 
+Показывает товар, если ввести ИД категории непосредственно, то есть, если вводится ИД категории самого первого уровня, то данный курсор не проходит по всем дочерним элементам.
+Курсор, оторый по ИД категории отдает список:  
+ИД товара, 
+Название товара,
+Текущая цена */
+create or replace procedure get_info_goods_from_id_cat (p_category_goods_id  in   integer, 
+                                                        refcur               out  sys_refcursor) 
+is
+
+begin
+
+  open refcur for
+    select
+      goods.goods_id,
+      goods.goods_name,
+      goods_price.price
+    from
+      category_goods,
+      goods_inherit_category, 
+      goods, 
+      goods_price
+    where
+      category_goods.category_goods_id = p_category_goods_id and
+      category_goods.category_goods_id = goods_inherit_category.category_goods_id and
+      goods_inherit_category.goods_id = goods.goods_id and
+      goods.goods_id = goods_price.goods_id and
+      (sysdate between goods_price.active_from and goods_price.active_to); /* Цена товара должна попадать в промежуток времени */
+  
+end get_info_goods_from_id_cat;
+
+
+/* Вызов процедуры между begin и end */
+declare
+  tmp$cur sys_refcursor;
+  goods_id integer;
+  goods_name varchar2(200);
+  price number;
+begin
+
+  /* Вызов происходит здесь! */
+  get_info_goods_from_id_cat (7,tmp$cur);
+
+  loop
+    fetch 
+      tmp$cur
+    into  
+      goods_id, goods_name, price;
+    
+    exit when tmp$cur%notfound;
+    
+    dbms_output.put_line('ИД товара: ' || goods_id ||
+             ',        Название товара: ' || goods_name ||
+             ',        текущая цена товара: ' || price);
+  end loop;
+end;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+																					/* 2. Версия 3. 
+																					Если вводится ИД категории самого первого уровня, то пройти по всем дочерним элементам.
+																					Курсор, оторый по ИД категории отдает список:  
+																					ИД товара, 
+																					Название товара,
+																					Текущая цена */
+
+
+
+/* 2. Версия 1. 
+Отдает иерархический список категорий, начиная с тех, в которых есть товары с заведенной стоимостью. 
+Иерархический список категорий следующий: 
+ИД категории
+ИД родительской категории
+Название
+Флаг, который показывает является ли узел листом */
+declare
+  
+  cursor cursor_category_info is
+    select
+	  category_goods_id,
+	  parent_category_goods_id,
+	  category_goods_name
+	from
+	  category_goods;
+       
+      
+      
+  v_cursor_category_info  cursor_category_info%ROWTYPE;
+begin
+  
+  
+  /* Возможно, можно использовать представленя */
+  /* exists или not exists */
+  
+  
+  
+  open cursor_category_info;
+  
+  loop
+  
+    fetch 
+      cursor_category_info 
+    into 
+      v_cursor_category_info;
+      
+    exit when (cursor_category_info%notfound); 
+	
+	
+	select * 
+	from 
+	where 
+		category_goods_id <> 
+      
+    dbms_output.put_line('ИД товара: ' ||  v_cursor_category_info.category_goods_id ||
+             ',        Название товара: ' ||  v_cursor_category_info.parent_category_goods_id ||
+             ',        текущая цена товара: ' || v_cursor_category_info.category_goods_name);
+            
+  end loop;
+                
+  close cursor_category_info;
 end;
 
 
