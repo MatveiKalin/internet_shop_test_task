b1679528d080a692de3abac38b3fd409c9fa1215
Калин Матвей Викторович <kalin@MOON.LAN>
2019-07-12 11:37:39 +0500

    Появилась процедура в пакете pkg_goods для чтого, чтобы пользователь мог ввести  ИД категории самого первого уровня, а затем запрос проходится по всем дочерним элементам.

--- Запросы/4. Пакеты.sql
+++ Запросы/4. Пакеты.sql
@@ -1,18 +1,22 @@
 /* Создание спецификации пакета */
 create or replace package pkg_goods is
 
-	procedure get_info_goods_from_id_cat (p_category_goods_id  in   integer, 
-										  refcur               out  sys_refcursor);
-								   
-	
-	procedure get_info_goods (p_goods_id  in   integer, 
-							  refcur      out  sys_refcursor);
-							  
-							  
-	procedure add_goods_to_basket (p_users_id       in integer, 
-								   p_goods_price_id in integer,
-								   p_amount         in integer);
-								   
+  procedure get_info_goods_from_id_cat (p_category_goods_id  in   integer, 
+										refcur               out  sys_refcursor);
+                      
+                      
+  procedure get_info_goods_from_id_cat_v2 (p_category_goods_id  in   integer, 
+										   refcur               out  sys_refcursor); 
+                   
+  
+  procedure get_info_goods (p_goods_id  in   integer, 
+							refcur      out  sys_refcursor);
+                
+                
+  procedure add_goods_to_basket (p_users_id       in integer, 
+								 p_goods_price_id in integer,
+								 p_amount         in integer);
+                   
 end pkg_goods;
 
 
@@ -21,7 +25,7 @@ end pkg_goods;
 /* Создание тела пакета */
 create or replace package body pkg_goods is
 
-  /* Процедура, а вней курсор, который показывает товар, если ввести ИД категории непосредственно, 
+  /* 2. Процедура, а вней курсор, который показывает товар, если ввести ИД категории непосредственно, 
   то есть, если вводится ИД категории самого первого уровня, то данный курсор не проходит по всем дочерним элементам.
   Курсор, оторый по ИД категории отдает список:  
   ИД товара, 
@@ -54,7 +58,46 @@ create or replace package body pkg_goods is
   
   
   
-  /* Процедура, а вней курсор, который по ИД товара одает данные: 
+  /* 2. Версия 2 (Если вводится ИД категории самого первого уровня, то пройти по всем дочерним элементам).
+  Процедура, а вней курсор, который по ИД категории отдает список:  
+  ИД товара, 
+  Название товара,
+  Текущая цена */
+  procedure get_info_goods_from_id_cat_v2 (p_category_goods_id  in   integer, 
+                                           refcur               out  sys_refcursor) 
+  is
+
+  begin
+
+    open refcur for
+    select
+      goods.goods_id,
+      goods.goods_name,
+      goods_price.price
+    from
+      category_goods,
+      goods_inherit_category, 
+      goods, 
+      goods_price
+    where
+      category_goods.category_goods_id in (select
+												category_goods_id
+											from
+												category_goods
+											start with 
+												parent_category_goods_id = p_category_goods_id
+											connect by 
+												prior category_goods_id = parent_category_goods_id) and
+      category_goods.category_goods_id = goods_inherit_category.category_goods_id and
+      goods_inherit_category.goods_id = goods.goods_id and
+      goods.goods_id = goods_price.goods_id and
+      (sysdate between goods_price.active_from and goods_price.active_to); /* Цена товара должна попадать в промежуток времени */
+    
+  end get_info_goods_from_id_cat_v2;
+  
+  
+  
+  /* 3.Процедура, а вней курсор, который по ИД товара одает данные: 
   название товара, 
   текущая цена товара,
   длинна товара,
@@ -84,7 +127,7 @@ create or replace package body pkg_goods is
   end get_info_goods;
   
   
-  /* Процедура, которая добавляет товар в корзину */
+  /* 4. Процедура, которая добавляет товар в корзину */
   procedure add_goods_to_basket (p_users_id       in integer, 
                                  p_goods_price_id in integer,
                                  p_amount         in integer) 
@@ -117,6 +160,33 @@ begin
 
   /* Вызов происходит здесь! */
   pkg_goods.get_info_goods_from_id_cat (7,tmp$cur);
+
+  loop
+    fetch 
+      tmp$cur
+    into  
+      goods_id, goods_name, price;
+    
+    exit when tmp$cur%notfound;
+    
+    dbms_output.put_line('ИД товара: ' || goods_id ||
+						 ',        Название товара: ' || goods_name ||
+						 ',        текущая цена товара: ' || price);
+  end loop;
+end;
+
+
+
+/* Вызов процедуры "get_info_goods_from_id_cat_v2" из пакета между begin и end */
+declare
+  tmp$cur sys_refcursor;
+  goods_id integer;
+  goods_name varchar2(200);
+  price number;
+begin
+
+  /* Вызов происходит здесь! */
+  pkg_goods.get_info_goods_from_id_cat_v2 (2, tmp$cur); /* Если ввести в первый параметр аргумент 2, то выведутся вся обувь. А если ввести в первый параметр аргумент 3, то выведется все штаны. */
 
   loop
     fetch 
